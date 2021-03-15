e69945d942a407d38e9cf1e49ce1ddaf33d04ed2
Abhiram <abhiram.vadlapatla@philips.com>
2021-03-16 01:41:46 +0530

    Initial Commit

---
+++ FaceSwap/FaceSwapApp.swift
@@ -0,0 +1,17 @@
+//
+//  FaceSwapApp.swift
+//  FaceSwap
+//
+//  Created by Abhiram on 16/03/21.
+//
+
+import SwiftUI
+
+@main
+struct FaceSwapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
